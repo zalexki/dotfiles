1fb8daa3bde9e3853578f82f6cd546e14f3bb7ff
Alexis Haumaitre <haumalex@gmail.com>
2016-12-11 17:34:02 +0100

    Create foldOnLoadFile.py

---
+++ sublimeText3/foldOnLoadFile.py
@@ -0,0 +1,7 @@
+import sublime, sublime_plugin
+
+class EventListener( sublime_plugin.EventListener ):
+
+    def on_load ( self, view ):
+    
+        view.run_command ( "fold_by_level", { "level": 2 } )
