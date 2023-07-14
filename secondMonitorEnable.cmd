8ac773b2bb131360ba5b3060741e04d734a16532
Alex. H <haumalex@gmail.com>
2023-07-14 14:33:42 +0200

    Create secondMonitorEnable.cmd

---
+++ secondMonitorEnable.cmd
@@ -0,0 +1,6 @@
+Tool to DL -> https://www.nirsoft.net/utils/multi_monitor_tool.html
+File to place here -> C:\Users\{USERNAME}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+
+D: 
+MultiMonitorTool.exe /enable 3
+MultiMonitorTool.exe /disable 2
